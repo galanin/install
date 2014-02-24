98140fe11098ce86806ba06719f01afe36bbae1b
Serge Galanin <s.galanin@gmail.com>
2014-02-24 09:59:02 +0400

    htpc transmission config

--- manifests/transmission.pp
+++ manifests/transmission.pp
@@ -11,4 +11,23 @@ class install::transmission {
     require => Package['transmission-daemon'],
   }
 
+  file {'/etc/default/transmission-daemon':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('install/etc/default/transmission-daemon.erb'),
+    require => Package['transmission-daemon'],
+    notify  => Service['transmission-daemon'],
+  }
+
+  file {'/etc/init/transmission-daemon.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('install/etc/init/transmission-daemon.conf.erb'),
+    require => Package['transmission-daemon'],
+    notify  => Service['transmission-daemon'],
+  }
 }
